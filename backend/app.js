3198d38268497eb7b1f0114aae0d6d91ee49be94
OfekMajar <ofek.majar@gmail.com>
2024-03-04 16:44:43 +0200

    db connected

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,19 @@
 const express = require("express");
-const studentRouter = require("./routes/student.routes");
-const hrRouter = require("./routes/hr.routes");
-const staffRouter = require("./routes/staff.routes");
-const collageRouter = require("./routes/groceryList.routes");
-const jobRouter = require("./routes/tokenManipulation.routes");
+// const studentRouter = require("./routes/student.routes");
+// const hrRouter = require("./routes/hr.routes");
+// const staffRouter = require("./routes/staff.routes");
+// const collageRouter = require("./routes/groceryList.routes");
+// const jobRouter = require("./routes/tokenManipulation.routes");
 const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use("/api/v1/students", studentRouter);
-app.use("/api/v1/hrs", hrRouter);
-app.use("/api/v1/staffs", staffRouter);
-app.use("/api/v1/collages", collageRouter);
-app.use("/api/v1/jobs",jobRouter)
+// app.use(cors());
+// app.use(express.json());
+// app.use("/api/v1/students", studentRouter);
+// app.use("/api/v1/hrs", hrRouter);
+// app.use("/api/v1/staff", staffRouter);
+// app.use("/api/v1/collages", collageRouter);
+// app.use("/api/v1/jobs",jobRouter)
 
 module.exports = { app };
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 const {app} = require("./app")
 const mongoose=require("mongoose")
-const {config}=require("./config")
-const {MONGO_URL}=config
+// const {config}=require("./config")
+// const {MONGO_URL}=config
 
 mongoose
-  .connect(MONGO_URL,{socketTimeoutMS: 45000})
+  .connect("mongodb+srv://ofekmajardev:XL8B5YSnn18B5Ya3@cluster0.ws5izty.mongodb.net/Skillable?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => {
     console.log("connected to db");
   })
