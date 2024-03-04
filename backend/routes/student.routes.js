c809bc4a4a14610eec1871e056303c4132d085b5
sagi pacharkovsky <139847862+sagipacha@users.noreply.github.com>
2024-03-04 16:22:32 +0200

    routes

---
+++ backend/routes/student.routes.js
@@ -0,0 +1,31 @@
+const { Router } = require("express")
+const router = Router();
+
+//Create a new student
+router.post('/', createNewStudent);
+
+//^ register
+router.post("/register", register);
+
+//^ login
+router.post("/login", login);
+
+//get a student by id
+router.get("/:id", getAllStudentById);
+
+//get all students
+router.get("/", getAllStudents);
+
+//update student by id
+router.patch("/:id", updateUser);
+
+//delete student by id
+router.delete("/:id", deleteUser);
+
+
+module.exports = router;
+
+
+
+
+
