ede4d775a028165f676a4b7f08557d4c4c360c5a
OfekMajar <ofek.majar@gmail.com>
2024-03-04 15:46:12 +0200

    addedindex

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,19 @@
-const ex = 2;
-const e = 5;
-let f = 5;
+const {app} = require("./app")
+const mongoose=require("mongoose")
+const {config}=require("./config")
+const {MONGO_URL}=config
+
+mongoose
+  .connect(MONGO_URL,{socketTimeoutMS: 45000})
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+ 
+const PORT = process.env.PORT || 1010;
+app.listen(PORT, () => {
+  console.log(`the server is running on port ${PORT}`);
+});
