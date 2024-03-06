3ceed521886c1cd7141b20ae57df89484b4ffb66
Matantalmid <matant1999@gmail.com>
2024-03-06 12:24:31 +0200

    home page

--- fronend/src/Components/Home/JobsStatus/JobsStatus.jsx
+++ fronend/src/Components/Home/JobsStatus/JobsStatus.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCircle,
-} from "@fortawesome/free-solid-svg-icons";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
+import styles from "./JobsStatus.module.css";
 function JobsStatus() {
   return (
     <>
-      <div>
-        <div>
+      <div className={styles.jobsStatusContainer}>
+        <div className={styles.containerOne}>
           <h1>סטטוס משרות</h1>
           <select name="">
             <option value="month">חודש אחרון</option>
@@ -15,15 +14,15 @@ function JobsStatus() {
             <option value="year">שנה האחרונה</option>
           </select>
         </div>
-        <div>
+        <div className={styles.containerTwo}>
           <FontAwesomeIcon icon={faCircle} style={{ color: "#6940DB" }} />
           <span>בתהליך</span>{" "}
           <FontAwesomeIcon icon={faCircle} style={{ color: "#9375e6" }} />
           <span>פרסמו</span>{" "}
           <FontAwesomeIcon icon={faCircle} style={{ color: "#F0ECFB" }} />
-        </div>
         <span>התקבלו</span>{" "}
-        <div>
+        </div>
+        <div className={styles.containerThree}>
           <img
             src="https://consumersiteimages.trustpilot.net/business-units/5c61aa30923a6e0001c3c22b-198x149-1x.avif"
             alt="diagram"
