78f8a6acd02ee08b4aa6fbde64600ffa0e1f70b8
OfekMajar <ofek.majar@gmail.com>
2024-03-12 13:42:07 +0200

    skills model

--- backend/models/Skill.model.js
+++ backend/models/Skill.model.js
@@ -1,15 +1,9 @@
 const mongoose = require("mongoose");
 
-const collageSchema = new mongoose.Schema({
-  firstName: { type: String, require: true },
-  lastName: { type: String, require: true },
-  password: { type: String, require: true },
-  email: { type: String, require: true },
-  profession: { type: String },
-  coverImg: { type: String },
-  graduates: [{ type: mongoose.Types.ObjectId, ref: "Students" }],
-  releventJobs: [{ type: mongoose.Types.ObjectId, ref: "Job" }],
+const skillSchema = new mongoose.Schema({
+  name: { type: String, require: true },
+  type: { type: String},
 });
 
-const Collage = mongoose.model("Collage", collageSchema);
-module.exports = { Collage };
+const Skill = mongoose.model("Skill", skillSchema);
+module.exports = { Skill };
